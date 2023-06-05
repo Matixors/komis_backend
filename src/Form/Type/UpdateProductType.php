2b8da29a2227cef63be2cac60daaef55784faf7b
Matixors <mbernacki.developer@gmail.com>
2023-06-05 18:43:06 +0200

    CRUD

---
+++ src/Form/Type/UpdateProductType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdateProduct extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('make', TextType::class)
+            ->add('model', TextType::class)
+            ->add('location', TextType::class)
+            ->add('price', IntegerType::class)
+            ->add('description', TextareaType::class)
+            ->add('registerNumber', TextType::class)
+            ->add('productionYear', IntegerType::class)
+            ->add('colour', TextType::class)
+            ->add('crashed', CheckboxType::class, [
+                "false_values"=>["No", "False"]
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Product::class,
+            'csrf_protection' => false
+        ));
+    }
+}
